4ae772b9cab87303dba2d34a8e2f0e55de30a95d
Richard Garbi <richard@mobilgov.com>
2021-09-03 10:15:59 -0500

    working

--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -1,14 +1,18 @@
 use tokio::net::TcpListener;
 use tokio::io::{AsyncWriteExt, AsyncBufReadExt};
 use tokio::io::{BufReader};
+use tokio::sync::broadcast;
 
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8000").await.unwrap();
+    let (tx, mut _rx) = broadcast::channel(10);
 
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
@@ -17,13 +21,22 @@ async fn main() {
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
-                }
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
 
-                writer.write_all(&line.as_bytes()).await.unwrap();
-                line.clear();
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
             }
         });
     }
