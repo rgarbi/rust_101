5f81db3aa21ce528d0b07de693af5f3e0b7731b8
Richard Garbi <richard@mobilgov.com>
2021-09-03 09:48:06 -0500

    getting it going

--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -1,5 +1,32 @@
+use tokio::net::TcpListener;
+use tokio::io::{AsyncWriteExt, AsyncBufReadExt};
+use tokio::io::{BufReader};
 
 
+#[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:8000").await.unwrap();
+
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+
+                writer.write_all(&line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
+    }
 }
+
+
